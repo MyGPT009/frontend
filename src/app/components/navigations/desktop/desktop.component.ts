49d2184ebd0db890144b5c7c20cde40b760aef43
thibaut009 <gabet.thibaut@gmail.com>
2025-05-09 15:41:54 +0200

    Update desktop navigation

--- src/app/components/navigations/desktop/desktop.component.ts
+++ src/app/components/navigations/desktop/desktop.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { APP_ROUTES } from '../../../constants/routes';
 
 @Component({
   selector: 'app-desktop',
@@ -7,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './desktop.component.css'
 })
 export class DesktopComponent {
+  constructor(
+    private router: Router
+  ) {}
 
+  goToHome(): void {
+    this.router.navigate([APP_ROUTES.HOME]);
+  }
+
+  goToSetting(): void {
+    this.router.navigate([APP_ROUTES.SETTING]);
+  }
+
+  goToProfile(): void {
+    this.router.navigate([APP_ROUTES.PROFILE]);
+  }
 }
