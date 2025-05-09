0f4b9c3fc2a495e04067a0998e920140f03a78aa
thibaut009 <gabet.thibaut@gmail.com>
2025-05-09 15:21:50 +0200

    Add desktop navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DesktopComponent } from './components/navigations/desktop/desktop.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DesktopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
